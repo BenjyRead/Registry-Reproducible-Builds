688a2403e268b49fd0a236135a4d76985214c15c
Benjamin Read <benjamin.sebastien.read@gmail.com>
2026-02-18 18:16:36 +0000

    initial github actions workflow created

---
+++ README.md
@@ -0,0 +1,1 @@
+# Reproducible Build Component
--- npmjs/svelte/5.48.2/generator.go
+++ npmjs/svelte/5.48.2/generator.go
@@ -9,13 +9,14 @@ type ContainerConfig struct {
 	NODE_IMAGE       string
 	REPO_URL         string
 	COMMIT_SHA       string
+	BUILD_DIR        string
 	OFFICIAL_TGZ_URL string
 	TGZ_FILENAME     string
 	ALPINE_IMAGE     string
 }
 
 func main() {
-	bytes, error := os.ReadFile("cmd/rep-build/templates/Svelte_PNPM_Containerfile")
+	bytes, error := os.ReadFile("cmd/rep-build/templates/PNPM_Containerfile")
 	if error != nil {
 		println("Error reading template file:", error.Error())
 		return
@@ -28,6 +29,7 @@ func main() {
 		ALPINE_IMAGE:     "docker.io/library/alpine:3.23",
 		REPO_URL:         "https://github.com/sveltejs/svelte.git",
 		COMMIT_SHA:       "e1427aa0c8b5abe88aea972ea397ea499cb8f2db",
+		BUILD_DIR:        "packages/svelte",
 		OFFICIAL_TGZ_URL: "https://registry.npmjs.org/svelte/-/svelte-5.48.2.tgz",
 		TGZ_FILENAME:     "svelte-5.48.2.tgz",
 	}
@@ -39,7 +41,12 @@ func main() {
 		println("Error creating Containerfile:", err.Error())
 		return
 	}
-	defer containerfile.Close()
+
+	defer func() {
+		if cerr := containerfile.Close(); cerr != nil {
+			println("Error closing Containerfile:", cerr.Error())
+		}
+	}()
 
 	if err := t.Execute(containerfile, data); err != nil {
 		println("Error executing template:", err.Error())
